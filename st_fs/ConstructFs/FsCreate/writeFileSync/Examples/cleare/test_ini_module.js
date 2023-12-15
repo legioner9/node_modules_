cddd400aa365e55ccd0a6e00dd2d94bab50512a4
[alt]legioner9 <legioner9@inbox.ru>
2023-12-15 23:07:48 +0700

    <> ##

--- st_fs/ConstructFs/FsCreate/writeFileSync/Examples/cleare/test_ini_module.js
+++ st_fs/ConstructFs/FsCreate/writeFileSync/Examples/cleare/test_ini_module.js
@@ -1,12 +1,11 @@
-const Path = require ( 'path' );
-const s_Fs = require ( 'st_ini_fs' );
-const { arht } = require ( 'st_ini_arht' );
+const Path = require('path');
+const s_Fs = require('st_ini_fs');
+const { arht } = require('st_ini_arht');
 
-const arr_names = Object.getOwnPropertyNames ( s_Fs );
-arr_names.map ( item => arht.before ( s_Fs[item], module ) );
+const arr_names = Object.getOwnPropertyNames(s_Fs);
+arr_names.map((item) => arht.before(s_Fs[item], module));
 
 const { s_writeFileSync } = s_Fs;
-
 
 // BOUNDEN CALL define module
 // arht.before ( s_writeFileSync , module);
@@ -18,6 +17,9 @@ const { s_writeFileSync } = s_Fs;
 s_writeFileSync.mode.logFs = true;
 // s_writeFileSync.mode.stack = true;
 
-s_writeFileSync ( Path.join ( __dirname, 'file_for_test' ), 'content file_for_test\n' );
+s_writeFileSync(
+  Path.join(__dirname, 'file_for_test'),
+  'content file_for_test\n',
+);
 
-debugger
+debugger;
